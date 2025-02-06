98b3a022281169bd050dcb5e847fbccd014ad6ba
benjuwan <benjuwan.banquet12@gmail.com>
2025-02-06 11:14:27 +0900

    fix:先般追加した文字列配列内の各文字列の重複計測処理の修正とREADMEの更新.

--- libs/checkDuplicateWordAndCounts/checkDuplicateWordAndCounts.ts
+++ libs/checkDuplicateWordAndCounts/checkDuplicateWordAndCounts.ts
@@ -1,4 +1,5 @@
 type checkDuplicateWordAndCounts_resultType = {
+    chars: string;
     char: string;
     count: number;
 }
@@ -24,18 +25,26 @@ const checkDuplicateWordAndCounts: (theWords: string[]) => checkDuplicateWordAnd
 
         // エントリー（[key, value]のペア）を取得
         for (const [char, count] of charsMap.entries()) {
+            // 文字が切り替わったら重複計測回数（theCount）を初期化
+            if (theChar !== char) {
+                theCount = 0;
+            }
+
+            // 重複計測回数（theCount）より大きい（＝重複している）場合は購読開始
             if (count > theCount) {
                 theChar = char;
                 theCount = count;
             }
-        }
 
-        // 重複がある場合のみ（出現回数が2回以上）結果に追加
-        if (theCount >= 2) {
-            results.push({
-                char: theChar,
-                count: theCount
-            });
+            // 重複がある場合のみ（出現回数が2回以上）結果に追加
+            if (theCount >= 2) {
+                const newResult: checkDuplicateWordAndCounts_resultType = {
+                    chars: theWord,
+                    char: theChar,
+                    count: theCount
+                }
+                results.push(newResult);
+            }
         }
     }
 
@@ -45,5 +54,4 @@ const checkDuplicateWordAndCounts: (theWords: string[]) => checkDuplicateWordAnd
 const targetStrAry: string[] = ["beer", "www", "apple", "banana", "soda", "benjuwan jijao"];
 
 const theCheckDuplicateWordAndCounts: checkDuplicateWordAndCounts_resultType[] = checkDuplicateWordAndCounts(targetStrAry);
-
 console.log(theCheckDuplicateWordAndCounts);
