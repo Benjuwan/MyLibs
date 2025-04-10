7c4f050d667c3b7871853f46c836a3e3aca2f3b0
benjuwan_5931 <benjuwan.kokusaig@gmail.com>
2025-04-10 12:00:37 +0900

    frontend のカレンダー機能の input タグのスタイリング調整（safari対策）.

--- frontend/theCalendar/src/schedule/todoItems/TodoForm.tsx
+++ frontend/theCalendar/src/schedule/todoItems/TodoForm.tsx
@@ -79,14 +79,14 @@ export const TodoForm = ({ props }: { props: TodoFormType }) => {
                     <input
                         type="time"
                         id="startTime"
-                        className="block text-[1rem] leading-[2] border border-[#919191] bg-white rounded w-full min-w-[calc(100vw/3)] pl-[.25em] md:min-w-auto"
+                        className="appearance-none block text-[1rem] leading-[2] border border-[#919191] bg-white rounded w-full pl-[.25em]"
                         value={todoItems.startTime}
                         onChange={(e: ChangeEvent<HTMLInputElement>) => handleFormEntries<todoItemType>(e, todoItems, setTodoItems)} /></label>
                 <label className="block w-full text-left mb-[1em] text-[clamp(0.625rem,100%,0.875rem)] my-[1em]">終了時刻
                     <input
                         type="time"
                         id="finishTime"
-                        className="block text-[1rem] leading-[2] border border-[#919191] bg-white rounded w-full min-w-[calc(100vw/3)] pl-[.25em] md:min-w-auto"
+                        className="appearance-none block text-[1rem] leading-[2] border border-[#919191] bg-white rounded w-full pl-[.25em]"
                         value={todoItems.finishTime}
                         onChange={(e: ChangeEvent<HTMLInputElement>) => handleFormEntries<todoItemType>(e, todoItems, setTodoItems)} /></label>
             </div>
