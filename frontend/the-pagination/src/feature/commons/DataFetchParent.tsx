3bc1ebd649905ff37d1a93a9b19758fffcc1bfaa
benjuwan_5931 <benjuwan.kokusaig@gmail.com>
2025-04-17 16:11:02 +0900

    fix: ページネーションコンポーネントのアップデート及びフォルダ構成の整理、個人用学習ドキュメントの更新.

--- frontend/the-pagination/src/feature/commons/DataFetchParent.tsx
+++ frontend/the-pagination/src/feature/commons/DataFetchParent.tsx
@@ -1,6 +1,6 @@
 import { memo, Suspense } from "react";
-import { jsonPostType } from "@/ts/json-post";
 import Loading from "@/app/loading";
+import { jsonPostType } from "../ts/json-post";
 import DataFecthChild from "./DataFecthChild";
 
 function TheDataFetchParent() {
@@ -8,13 +8,6 @@ function TheDataFetchParent() {
 
     // ※ await はしない。Promise を返す記述にする。Promise が未完了ならサスペンド状態となる（Suspense の fallback が返る） 
     const fetchdataPromise: Promise<jsonPostType[]> = fetch(fetchPathUrl).then(res => res.json());
-
-    // デモとして、あえて遅延処理を実施（※ Promiseインスタンスを使って、解決処理にフェッチしたデータ内容を格納した Promise として返却）
-    // const fetchdataPromise: Promise<jsonPostType[]> = new Promise(resolve => {
-    //     setTimeout(() => {
-    //         fetch(fetchPathUrl).then(res => res.json()).then(data => resolve(data));
-    //     }, 3000);
-    // });
 
     return (
         <main className="w-[clamp(20rem,100%,60rem)] my-[5em] mx-auto p-[1.5em]">
